c9e886ecffcb5998e0ce7f9d3b304694aa8fb15b
Camilo_Tabares <camilo.tabares.84@gmail.com>
2019-03-11 15:49:59 -0500

    step 3 -adding Index at Route as a functional component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/layout/NavBar';
+import Index from './components/layout/Index'
 
 class App extends Component {
   render() {
@@ -9,6 +10,11 @@ class App extends Component {
       <Router>
       <>
         <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+        </div>
       </>
       </Router>
     );
