ae95249c565198ebbc10678d0ecc7f000f075083
Camilo_Tabares <camilo.tabares.84@gmail.com>
2019-03-13 09:40:51 -0500

    run build

---
+++ build/precache-manifest.42062caf2e803bd70d1eaff930d59e61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3868b519e0c4d30e3352c022b0782ce",
+    "url": "/music_app_react/static/media/spinner.d3868b51.svg"
+  },
+  {
+    "revision": "010ce4d02a46e960bd65",
+    "url": "/music_app_react/static/js/runtime~main.544b4ca1.js"
+  },
+  {
+    "revision": "794eca48117deccd4796",
+    "url": "/music_app_react/static/js/main.9c4bdc31.chunk.js"
+  },
+  {
+    "revision": "0983e4880feaaaed1511",
+    "url": "/music_app_react/static/js/2.22395e2f.chunk.js"
+  },
+  {
+    "revision": "794eca48117deccd4796",
+    "url": "/music_app_react/static/css/main.6669a650.chunk.css"
+  },
+  {
+    "revision": "99c02160353d58edfe08153a6624227b",
+    "url": "/music_app_react/index.html"
+  }
+];
